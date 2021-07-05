74ff2e29073c3a5a36559dea60aadd8cc54c5817
GabytaDev <gabyta.loyola@gmail.com>
2021-07-05 14:48:25 -0300

    ejercicio 12

---
+++ Funciones ejercicio 12/index.js
@@ -0,0 +1,14 @@
+// Definí una función calcularPerimetroRectangulo que reciba como argumentos 
+// el ancho y el alto de un rectángulo y devuelva su perímetro
+
+//  calcularPerimetroRectangulo(3.2, 5)
+// 16.4
+//  calcularPerimetroRectangulo(10, 20)
+// 60
+
+const calcularPerimetroRectangulo = (ancho, alto) => {
+    return ((ancho + alto)*2)
+}
+
+calcularPerimetroRectangulo (3.2, 5);
+calcularPerimetroRectangulo (10, 20);
