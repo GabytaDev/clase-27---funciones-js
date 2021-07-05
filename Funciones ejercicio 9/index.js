8180d11a74b9c813f63fb05911fc175a236c10ec
GabytaDev <gabyta.loyola@gmail.com>
2021-07-05 14:34:15 -0300

    ejercicio 9

---
+++ Funciones ejercicio 9/index.js
@@ -0,0 +1,11 @@
+// Usando las funciones anteriores (obtenerNombreCompleto, saludar y gritar), definí una 
+// función saludarGritando que reciba como argumentos un nombre y un apellido y 
+// devuelva un saludo con signos de exclamación.
+
+//  saludarGritando('Ada', 'Lovelace') 
+// ¡Hola Ada Lovelace, un gusto conocerte!
+
+const saludarGritando = (nombre,apellido) => {
+    return ("¡Hola "+ nombre + " "+ apellido + ", un gusto conocerte!")
+} 
+
