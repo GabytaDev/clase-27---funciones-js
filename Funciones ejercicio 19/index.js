9fd0f3d7eef600528e01f3931aafafe48a82b37c
GabytaDev <gabyta.loyola@gmail.com>
2021-07-07 16:11:46 -0300

    ejercicio 19 puntaje

---
+++ Funciones ejercicio 19/index.js
@@ -0,0 +1,35 @@
+// Definí una función calcularPuntaje que calcule el puntaje de un 
+// examen que consiste en ejercicios de distinto nivel. Debe recibir 
+// como argumento tres numeros que consisten en la cantidad de ejercicios resueltos 
+// en cada nivel y devolver un número con el puntaje correspondiente. 
+// El puntaje se calcula de la siguiente forma:
+
+// facil: 3 puntos
+// normal: 5 puntos
+// dificil: 10 puntos
+
+//  calcularPuntaje(3, 0, 0) 
+// 9
+//  calcularPuntaje(0, 2, 1) 
+// 20
+//  calcularPuntaje(5, 1, 2)
+// 40
+
+let facil = 3;
+let normal = 5;
+let dificil= 10;
+
+
+const calcularPuntaje = (num1, puntaje2, puntaje3) =>{
+    
+
+    return puntajeTotal ((puntaje1 * facil) + (puntaje2 * normal) + (puntaje3 * dificil))
+   
+}
+
+
+ calcularPuntaje(3, 0, 0) 
+
+ calcularPuntaje(0, 2, 1) 
+
+ calcularPuntaje(5, 1, 2)
